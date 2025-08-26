abbc5ff7e8d83e4aca75642a2adb0f3079c22fb4
Jethika Wanniarachchi <49968486+jethikaviduruwan@users.noreply.github.com>
2025-08-26 15:27:16 +0530

    init commit

---
+++ server/server/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <WinSock2.h>
+
+using namespace std;
+
+
+#pragma comment(lib,"ws2_32.lib")
+
+
+/*
+
+1. init winsock
+2. create socket
+3. get ip and port
+4. bind the ip/port with the socket
+5. listen on socket
+6. accept
+7. recall and send
+8. close the socket
+9. clean up winsok
+
+
+*/
+int main() {
+
+	return 0;
+}
